4cc7c97e87d3e613e986be17c07710922be575ac
beep-create <77546458+beep-create@users.noreply.github.com>
2021-05-29 13:23:38 -0400

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -35,53 +35,48 @@ document.getElementById("number_2").value = "";
 
 }
 
-function check() {
-   get_answer = document.getElementById("input_check_box").value;
-   if (get_answer == actual_answer)
-   {
-       if(answer_turn="player1")
-       {
-           update_player1_score = player1_score +1;
-           document.getElementById("player1_score").innerHTML = update_player1_score;
-           
-       }
-       else
-       {
-           update_player2_score = player2_score +1;
-           document.getElementById("player2_score").innerHTML = update_player2_score;
-           
-       }
-}
 
-if(question_turn == "player2") 
+function check()
 {
-question_turn = "player2"
-document.getElementById("player_question").innerHTML = "Question Turn - " + player2_name;
-
-}
-else
-
+get_answer = document.getElementById("input_check_box").value;
+console.log("france england denmark and spain");
+if(get_answer = actual_answer)  
 {
-question_turn = "player1"
-document.getElementById("player_question").innerHTML = "Question Turn - " + player1_name;
-
+    if(answer_turn == "player1")
+    {
+        player1_score = player1_score +1;
+        document.getElementById("player1_score").innerHTML = player1_score;
+    }
+    else 
+    {
+        player2_score = player2_score +1;
+        document.getElementById("player2_score").innerHTML = player2_score;
+    }
+}
+if(question_turn == "player1")
+{
+    question_turn = "player2"
+    document.getElementById("player_question").innerHTML = "Question Turn - " + player2_name ;
+}
+else 
+{
+    question_turn = "player1"
+    document.getElementById("player_question").innerHTML = "Question Turn - " + player1_name ;
 }
 
-//
-//
-
-}
-if(answer_turn == "player2") 
+if(answer_turn == "player1")
 {
     answer_turn = "player2"
-document.getElementById("player_answer").innerHTML = "Answer Turn - " + player2_name;
-
+    document.getElementById("player_answer").innerHTML = "Answer Turn - " + player2_name ;
 }
-else
-
+else 
 {
-answer_turn = "player1"
-document.getElementById("player_answer").innerHTML = "Answer Turn - " + player1_name;
-
+    answer_turn = "player1"
+    document.getElementById("player_answer").innerHTML = "Answer Turn - " + player1_name ;
 }
+
+document.getElementById("output").innerHTML = "";
+}
+
+
 
